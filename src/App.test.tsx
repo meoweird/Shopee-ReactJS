6874e12afc77a87755c8882d4d6e2eb8d34c4317
meoweird <meoweird.dev@gmail.com>
2024-01-18 11:47:46 +0700

    Fix lỗi liên quan đến Testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, test } from 'vitest'
 import { screen, waitFor } from '@testing-library/react'
 import * as matchers from '@testing-library/jest-dom/matchers'
+import '@testing-library/jest-dom/vitest'
 import { logScreen, renderWithRouter } from './utils/testUtils'
 import path from './constants/path'
 
@@ -9,30 +10,31 @@ expect.extend(matchers)
 describe('App', () => {
   test('App render và chuyển trang', async () => {
     const { user } = renderWithRouter()
-
+    // await logScreen()
     await waitFor(() => {
       expect(document.querySelector('title')?.textContent).toBe('Trang chủ | Shopee Clone')
     })
 
     await user.click(screen.getByText(/Đăng nhập/i))
     await waitFor(() => {
-      expect(screen.findByText('Bạn chưa có tài khoản?')).toBeTruthy()
+      expect(screen.findByText(/Bạn đã có tài khoản?/i)).toBeTruthy()
       expect(document.querySelector('title')?.textContent).toBe('Đăng nhập | Shopee Clone')
+    })
+  })
+
+  test('Về trang not found', async () => {
+    const badRoute = '/some/bad/route'
+    renderWithRouter({ route: badRoute })
+    await waitFor(() => {
+      expect(screen.getByText(/Page Not Found/i)).toBeInTheDocument()
     })
   })
 
   test('Render trang register', async () => {
     renderWithRouter({ route: path.register })
     await waitFor(() => {
-      expect(screen.findByText(/Bạn đã có tài khoản?/i)).toBeTruthy()
+      expect(screen.getByText(/Bạn đã có tài khoản?/i)).toBeInTheDocument()
     })
     // await logScreen()
-  })
-  test('Về trang not found', async () => {
-    const badRoute = '/some/bad/route'
-    renderWithRouter({ route: badRoute })
-    await waitFor(() => {
-      expect(screen.queryByText(/Page not found/i)).toBeTruthy()
-    })
   })
 })
--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -1,32 +1,47 @@
-import { screen, waitFor, fireEvent } from '@testing-library/react'
+import { screen, waitFor, fireEvent, cleanup } from '@testing-library/react'
 import path from 'src/constants/path'
 import { logScreen, renderWithRouter } from 'src/utils/testUtils'
-import { beforeAll, describe, expect, it } from 'vitest'
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import * as matchers from '@testing-library/jest-dom/matchers'
+import '@testing-library/jest-dom/vitest'
+
 expect.extend(matchers)
 
 describe('Login', () => {
-  beforeAll(async () => {
+  // let emailInput: HTMLInputElement
+  // let passwordInput: HTMLInputElement
+  // let submitButton: HTMLButtonElement
+  beforeEach(async () => {
     renderWithRouter({ route: path.login })
     await waitFor(() => {
-      expect(screen.queryByPlaceholderText('Email')).toBeTruthy()
+      expect(screen.queryByPlaceholderText('Email')).toBeInTheDocument()
     })
+    // emailInput = document.querySelector('form input[type="email"]') as HTMLInputElement
+    // passwordInput = document.querySelector('form input[type="password"]') as HTMLInputElement
+    // submitButton = document.querySelector('form button[type="submit"]') as HTMLButtonElement
+    // console.log(emailInput)
+    // await logScreen()
+  })
+  afterEach(() => {
+    cleanup()
   })
   it('Hiển thị lỗi required khi không nhập gì', async () => {
-    const submitButton = document.querySelector('form button[type="submit"]') as Element
+    const submitButton = document.querySelector('form button[type="submit"]') as HTMLButtonElement
+    // console.log(submitButton)
     fireEvent.submit(submitButton)
     await waitFor(async () => {
       expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
       expect(await screen.findByText('Password là bắt buộc')).toBeTruthy()
     })
   })
-  it('Hiển thị lỗi required khi không nhập gì', async () => {
-    const emailInput = document.querySelector('form input[type="email"]') as HTMLInputElement
-    const passwordInput = document.querySelector('form input[type="password"]') as HTMLInputElement
+  it('Hiển thị validate form', async () => {
+    const emailInput = document.querySelector('input[type="email"]') as HTMLInputElement
+    const passwordInput = document.querySelector('input[type="password"]') as HTMLInputElement
     const submitButton = document.querySelector('form button[type="submit"]') as HTMLButtonElement
+    // console.log('EMAIL INPUT HERE: \n' + emailInput)
     fireEvent.change(emailInput, {
       target: {
-        value: 'test@mail'
+        value: 'dong'
       }
     })
     fireEvent.change(passwordInput, {
@@ -35,10 +50,8 @@ describe('Login', () => {
       }
     })
     fireEvent.submit(submitButton)
-    await waitFor(() => {
-      expect(screen.queryByText('Email không đúng định dạng')).toBeTruthy()
-      expect(screen.queryByText('Độ dài từ 6 - 160 ký tự')).toBeTruthy()
-    })
+    expect(await screen.findByText(/Email không đúng định dạng/i)).toBeTruthy()
+    expect(await screen.findByText(/Độ dài từ 6 - 160 ký tự/i)).toBeTruthy()
     // await logScreen()
   })
 })
